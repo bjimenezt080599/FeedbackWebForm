3f89753a08d826c996213ea5c2865eeab1b2f095
BrendaJimenez <brenda.jimenez@titot.com.au>
2023-07-28 09:42:16 +1000

    Basic structure (HTTPClient still doesn't work, no style)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FeedbackFormComponent } from './feedback-form/feedback-form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedbackFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/feedback-form/feedback-form.component.ts
@@ -0,0 +1,47 @@
+// feedback-form.component.ts
+import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Component({
+  selector: 'app-feedback-form',
+  templateUrl: './feedback-form.component.html',
+  styleUrls: ['./feedback-form.component.css']
+})
+export class FeedbackFormComponent {
+  feedbackType: string = '';
+  source: string = '';
+  feedbackCategory: string = '';
+  feedbackSubCategory: string = '';
+  dateReceived: string = '';
+  text: string = '';
+  notes: string = '';
+  wantToBeContacted: boolean = false;
+
+  onSubmit() {
+    // Build the mutation to send the data to the GraphQL API
+    const mutation = `mutation feedbackMutation {
+      feedbackExternalCreate(detail: {
+        feedbackType: "${this.feedbackType}",
+        source: "${this.source}",
+        feedbackCategory: "${this.feedbackCategory}",
+        feedbackSubCategory: "${this.feedbackSubCategory}",
+        dateReceived: "${this.dateReceived}",
+        text: "${this.text}",
+        notes: "${this.notes}",
+        externalName: "${this.wantToBeContacted ? 'My name' : ''}"
+      }) { feedbackId }
+    }`;
+
+    // Make the GraphQL API call using HttpClient
+        this.httpClient.post('https://housing.logiqc.com.au/graphql', { query: mutation }, {
+      headers: {
+        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJUb2tlbklkIjoiM2QwM2NkMDYtMGRlZS00MjdjLWIzZDQtYjAyMzAwYzdiOWI3IiwiVXNlck5hbWUiOiJTeXN0ZW0iLCJEYXRlQ3JlYXRlZCI6IjIwMjMtMDYtMTZUMTI6MDc6MTAuNjg1NzEwNisxMDowMCIsIklzQXBpS2V5Ijp0cnVlLCJJc1NzbyI6ZmFsc2V9.w51Z5HxGMQo92N9z7Etc4kR_J8gsJ0CTSNe39nEzwbY',
+        'Content-Type': 'application/json'
+      }
+    }).subscribe((response: any) => {
+      console.log('Respuesta de la API:', response);
+    });
+  }
+
+  constructor(private httpClient: HttpClient) { }
+}
